2b18253287263193a7ae9df1ee7140028bed7805
Felipe Soares <soaresfelipef@gmail.com>
2016-09-01 00:25:04 -0300

    Button's actions were added

--- app/src/main/java/com/soaresfelipef/fitorflab/MainActivity.java
+++ app/src/main/java/com/soaresfelipef/fitorflab/MainActivity.java
@@ -2,12 +2,47 @@ package com.soaresfelipef.fitorflab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXERCISE_WEIGHTS = "Weight lifting";
+    public static final String EXERCISE_YOGA = "Yoga";
+    public static final String EXERCISE_CARDIO = "Cardio";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RelativeLayout weightButton = (RelativeLayout)findViewById(R.id.weightButton);
+        RelativeLayout lotusButton = (RelativeLayout)findViewById(R.id.yogaButton);
+        RelativeLayout cardioButton = (RelativeLayout)findViewById(R.id.cardioButton);
+
+        weightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadDetailActivity(EXERCISE_WEIGHTS);
+            }
+        });
+
+        lotusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadDetailActivity(EXERCISE_YOGA);
+            }
+        });
+
+        cardioButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadDetailActivity(EXERCISE_CARDIO);
+            }
+        });
+    }
+
+    private void loadDetailActivity(String exerciseTitle){
+
     }
 }
