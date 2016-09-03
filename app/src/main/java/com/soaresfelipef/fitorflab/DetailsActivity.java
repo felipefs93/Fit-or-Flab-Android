93f85e259fab5d701362a3442bd2841ba3dd4647
Felipe Soares <soaresfelipef@gmail.com>
2016-09-02 21:16:36 -0300

    Setup DetailActivity

--- app/src/main/java/com/soaresfelipef/fitorflab/DetailsActivity.java
+++ app/src/main/java/com/soaresfelipef/fitorflab/DetailsActivity.java
@@ -2,6 +2,8 @@ package com.soaresfelipef.fitorflab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        TextView exerciseTitle = (TextView)findViewById(R.id.exerciseTitle);
+        ImageView exerciseImg = (ImageView)findViewById(R.id.exerciseImage);
     }
 }
