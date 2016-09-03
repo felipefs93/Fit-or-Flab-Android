496c47cedbd5ddc7cc4fb787cc28f90214ce6690
Felipe Soares <soaresfelipef@gmail.com>
2016-09-02 21:22:36 -0300

    loadDetailActivity method was implemented

--- app/src/main/java/com/soaresfelipef/fitorflab/MainActivity.java
+++ app/src/main/java/com/soaresfelipef/fitorflab/MainActivity.java
@@ -1,5 +1,7 @@
 package com.soaresfelipef.fitorflab;
 
+import android.content.Intent;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +9,7 @@ import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_ITEM_TITLE = "extra.item.title";
     public static final String EXERCISE_WEIGHTS = "Weight lifting";
     public static final String EXERCISE_YOGA = "Yoga";
     public static final String EXERCISE_CARDIO = "Cardio";
@@ -43,6 +46,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadDetailActivity(String exerciseTitle){
-
+        Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+        intent.putExtra(MainActivity.EXTRA_ITEM_TITLE, exerciseTitle);
+        startActivity(intent);
     }
 }
